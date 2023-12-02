1cb2e6e8df7408de131b712bc2799e97ffe0a6b6
qwerty123456789012345678901 <143892277+qwerty123456789012345678901@users.noreply.github.com>
2023-12-02 14:32:03 +0300

    Добавление игрового окна и цикла   Code.py
    Добавление игрового окна и цикла

--- Code.py
+++ Code.py
@@ -1,6 +1,13 @@
 from pygame import *
+
+win_width = 700
+win_height = 500
+game = True
+time_delay = 15
+clock = time.Clock()
+
 class GameSprite(sprite.Sprite):
-    def __init__(self,player_image, player_x) ,player_y, width,height,min_x):
+    def __init__(self,player_image, player_x, player_y, width,height,min_x):
         super().__init__()
         self.width = width
         self.height = height
@@ -25,3 +32,22 @@ class Player(GameSprite):
             self.rect.x -= self.speed
         if keys_pressed[K_d] and self.rect.x < win_width-50:
             self.rect.x += self.speed
+
+
+
+
+window = display.set_mode((win_width,win_height))
+background = transform.scale(image.load("Black.jpg"),(win_width,win_height))
+
+
+while game:
+
+    keys_pressed = key.get_pressed()
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    window.blit(background,(0,0))
+
+    display.update()
+    time.delay(time_delay)
